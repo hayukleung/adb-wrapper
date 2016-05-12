7a8a26b952e1aaec373ffc935c9a53c4109d5083
HayukLeung <hayukleung@xThinker.local>
2016-05-12 15:50:48 +0800

    修改获取屏幕截图命令

--- screen-capture/get-screen-capture.sh
+++ screen-capture/get-screen-capture.sh
@@ -4,10 +4,14 @@ DEVICEID=$ANDROID_DEVICE_ID
 # 保存路径
 FILE=/mnt/sdcard/Pictures/Screenshots/_temp.png
 # 截屏
-shell.sh screencap -p $FILE
+if [ $# -ne 1 ]; then
+    shell.sh screencap -p $FILE
+else
+    adb -s $1 shell screencap -p $FILE
+fi
 # 保存至本地，默认桌面
 if [ $# -ne 1 ]; then
     adb.sh pull $FILE /Users/hayukleung/Desktop/
 else 
-    adb.sh pull $FILE $1
+    adb -s $1 pull $FILE /Users/hayukleung/Desktop/
 fi
